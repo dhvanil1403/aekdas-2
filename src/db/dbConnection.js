35d82ec4d372a2f629b3e6a877a23d8f1a945b13
kinjal-Vaishnav <kinjalvaishnav204@gmail.com>
2024-06-05 12:06:09 +0530

    commit

--- src/db/dbConnection.js
+++ src/db/dbConnection.js
@@ -7,7 +7,7 @@ const pool = new Pool({
 })
 
 pool.on('connect', () => {
-  // console.log('Connected to the database');
+  console.log('Connected to the database');
 });
 
 pool.on('error', (err) => {
