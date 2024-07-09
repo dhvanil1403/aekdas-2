24ec264bacb5c07d5d03f5d4984486b1254d05ed
aekads <aekads.in@gmail.com>
2024-07-09 14:26:16 +0530

    Update userLogin.controller.js

--- src/controllers/userLogin.controller.js
+++ src/controllers/userLogin.controller.js
@@ -1,28 +1,28 @@
-const userLogin = require("../models/Teams.model");
-const jwt = require("jsonwebtoken");
-const bcrypt = require("bcryptjs");
+// const userLogin = require("../models/Teams.model");
+// const jwt = require("jsonwebtoken");
+// const bcrypt = require("bcryptjs");
 
-const checkLogin = async (req, res) => {
+// const checkLogin = async (req, res) => {
 //   const { username, password } = req.body;
 //   console.log(username, password);
 //   try {
 //     const user = await userLogin.findMember(username, password);
 
-  //   if (user) {
-  //     const token = jwt.sign(
-  //       { id: user.id, username: user.username },
-  //       process.env.JWT_SECRET,
-  //       { expiresIn: process.env.JWT_EXPIRY }
-  //     );
-  // // console.log('loginTimeToken',token);
+//     if (user) {
+//       // const token = jwt.sign(
+//       //   { id: user.id, username: user.username },
+//       //   process.env.JWT_SECRET,
+//       //   { expiresIn: process.env.JWT_EXPIRY }
+//       // );
+//   // console.log('loginTimeToken',token);
       
-  //     res.cookie("token", token, {
-  //       httpOnly: true,
-  //       secure: true,        
-  //       sameSite: "strict",
-  //     });
-  //     // console.log('cookies',req.cookies);
-      res.redirect("/Dashboard");
+//       // res.cookie("token", token, {
+//       //   httpOnly: true,
+//       //   secure: true,        
+//       //   sameSite: "strict",
+//       // });
+//       // console.log('cookies',req.cookies);
+//       res.redirect("/Dashboard");
 //     } else {
 //       res.render("Login", { message: "Invalid login credentials" });
 //     }
@@ -30,7 +30,7 @@ const checkLogin = async (req, res) => {
 //     console.error("Error:", error);
 //     res.status(500).send("Failed to fetch users by findmember");
 //   }
-};
+// };
 // const dashboardAuth = (req, res, next) => {
 //   const token = req.cookies?.token;
 //   // console.log('dashboardAuthtoken',token);
@@ -53,7 +53,42 @@ const checkLogin = async (req, res) => {
 
 
 
+// module.exports = {
+//   checkLogin,
+//   dashboardAuth,
+// };
+const userLogin = require("../models/Teams.model");
+
+const checkLogin = async (req, res) => {
+  const { username, password } = req.body;
+  console.log(username, password);
+  try {
+    const user = await userLogin.findMember(username, password);
+    console.log("user",user);
+
+    if (user && user.memberpassword === password) {
+      // Store user info in session (assuming express-session is used)
+      req.session.user = {
+        id: user.id,
+        username: user.username,
+      };
+      res.redirect("/Dashboard");
+    } else {
+      res.render("Login", { message: "Invalid login credentials" });
+    }
+  } catch (error) {
+    console.error("Error:", error);
+    res.status(500).send("Failed to fetch users by findMember");
+  }
+};
+const dashboardAuth = (req, res, next) => {
+  if (req.session?.user) {
+    next();
+  } else {
+    res.redirect("/login");
+  }
+};
 module.exports = {
   checkLogin,
-  // dashboardAuth,
+  dashboardAuth,
 };
