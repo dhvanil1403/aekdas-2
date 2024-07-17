898d78c40a0b54f71a7d304927c4e22698bbda5c
aekads <aekads.in@gmail.com>
2024-07-17 18:19:06 +0530

    Update playlistRoutes.js

--- src/routes/playlistRoutes.js
+++ src/routes/playlistRoutes.js
@@ -14,8 +14,13 @@ router.get("/Photoes", library.getPhotoesForPlaylist);
 router.get("/Videos", library.getVideosForPlaylist);
 router.post("/UploadFile",upload.single('file'), library.handleUploadInDBForNewPlaylist);
 router.post('/createPlaylist',playlist.createPlaylist)
+router.post('/editPlaylist',playlist.editPlaylist)
 router.get("/PlaylistEdit/plylistEditScreen",playlist.showAvailableScreenForEditPlaylist)
 router.get("/PlaylistEdit/:playlistId",playlist.getPlaylistById)
+// router.get("/PlaylistDelete/:playlistId",playlist.deletePlaylist)
+router.get("/PlaylistDelete/:playlistId", playlist.deletePlaylist);
+
 router.get("/newPlaylist/selectScreens",playlist.showAvailableScreen)
+
 
 module.exports = router;
