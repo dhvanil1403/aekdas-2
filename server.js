18f826b2a714b790830105f3169f6f5b91e28c2e
aekads <aekads.in@gmail.com>
2024-07-25 15:11:37 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,7 +11,7 @@ const bodyParser = require('body-parser');
 const bcrypt = require('bcryptjs');
 const nodemailer = require('nodemailer');
 const { Sequelize, DataTypes } = require('sequelize');
-const authMiddleware = require('./src/middlewares/authMiddleware'); // Import authMiddleware
+
 
 const app = express();
 const api = require('./src/controllers/api.controller');
