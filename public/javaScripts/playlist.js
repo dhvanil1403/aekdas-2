68d570fce8906cf05c9028b2849b4ea8dbf81c17
aekads <aekads.in@gmail.com>
2024-07-30 11:49:58 +0530

    Add files via upload

---
+++ public/javaScripts/playlist.js
@@ -0,0 +1,21 @@
+function addPlaylistBox() {
+  document.getElementById("overlay").style.display = "flex";
+}
+function hideNewScreen() {
+  document.getElementById("overlay").style.display = "none";
+}
+document.getElementById("createPlaylistButton").addEventListener("click", function (event) {
+  const playlistName = document.getElementById("playlistName").value;
+  const playlistDescription = document.getElementById("playlistDescription").value;
+
+  if (!playlistName || !playlistDescription) {
+    alert("Please fill in all required fields.");
+    event.preventDefault();
+    return;
+  }
+
+  sessionStorage.setItem("playlistName", playlistName);
+  sessionStorage.setItem("playlistDescription", playlistDescription);
+
+  window.location.href = "/Dashboard/Playlist/newPlaylist";
+});
