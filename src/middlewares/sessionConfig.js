9ec11f25da78d22d84a2d72cf934f02520c65310
aekads <aekads.in@gmail.com>
2024-08-02 17:21:55 +0530

    Delete src directory

--- src/middlewares/sessionConfig.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-    secret: 'kinjal123',  // Use a strong secret in production
-    resave: false,
-    saveUninitialized: true,
-    cookie: { secure: false }  // Set to true if using HTTPS
-  };
